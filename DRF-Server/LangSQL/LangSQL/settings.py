6f4134d0b92de2b9eb8da83b88bab68f43b27cc3
Yash Parmar <yashparmar11y@gmail.com>
2025-03-22 02:42:39 +0530

    DRF-SERVER SETUP

--- DRF-Server/LangSQL/LangSQL/settings.py
+++ DRF-Server/LangSQL/LangSQL/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +81,14 @@ WSGI_APPLICATION = 'LangSQL.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'LangSQL',
+        'CLIENT':{
+            'host': os.getenv('DB_HOST'),
+            'name': 'LangSQL',
+            'authmechanism': 'SCRAM-SHA-1',
+            
+        }
     }
 }
 
